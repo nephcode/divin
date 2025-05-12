68cdf9ddad75d5bac3884fa719de9bdc78e1e8ae
Neph <nephcohen@gmail.com>
2025-05-12 23:48:11 +0200

    routes

--- app/routes.ts
+++ app/routes.ts
@@ -1,5 +1,5 @@
 import { type RouteConfig, route } from "@react-router/dev/routes";
 
 export default [
-    route( path: 'dashboard', file: 'routes/admin/dashboard.tsx')
+    route(path:'dashboard', file: 'routes/admin/dashboard.tsx')
 ] satisfies RouteConfig;
